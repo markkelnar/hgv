272f5a566b009283ddf3864fb85ce517f6e423de
Mark Kelnar <mark@wpengine.com>
2015-01-21 14:49:08 -0600

    Pass in domain variable to wordpress role

--- provisioning/roles/wordpress/templates/wp/local-config.php
+++ provisioning/roles/wordpress/templates/wp/local-config.php
@@ -32,8 +32,8 @@ $memcached_servers = array(
 );
 
 if( isset($_SERVER['HTTP_HOST']) && 'cache.' === substr( $_SERVER['HTTP_HOST'], 0, 6) ){
-    define('WP_SITEURL', 'http://cache.{{ enviro }}.hgv.dev');
-    define('WP_HOME', 'http://cache.{{ enviro }}.hgv.dev');
+    define('WP_SITEURL', 'http://cache.{{ domain }}');
+    define('WP_HOME', 'http://cache.{{ domain }}');
     define('WP_CACHE_KEY_SALT', 'cache_wpe_{{ enviro }}_1');
 }else{
     define('WP_CACHE_KEY_SALT', 'wpe_{{ enviro }}_1');
