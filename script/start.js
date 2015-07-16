4cea01602ea6a6f461e636e88b433c379cd4c81f
Eric Mann <eric@eamann.com>
2015-07-16 10:16:09 -0700

    Wire in prompts

--- script/start.js
+++ script/start.js
@@ -14,6 +14,7 @@ var fs = require( 'fs' ),
 	path = require( 'path' ),
 	extend = require( 'util' )._extend,
 	chalk = require( 'chalk' ),
+	prompt = require( 'prompt' ),
 	rootCheck = require( 'root-check' ),
 	yaml = require( 'yamljs' );
 
@@ -21,8 +22,12 @@ var fs = require( 'fs' ),
  * Default values for the YAML file
  */
 var defaults = {
-	environment: 'staging',
-	domain     : 'testing'
+	environ     : '',
+	hvvm_domains: [],
+	php_domains : [],
+	wpe_name    : '',
+	branch      : 'develop',
+	remote      : 'staging'
 };
 
 /**
@@ -80,10 +85,57 @@ function pre() {
  * Initialize the prompting machine.
  */
 function init() {
-	
+	var skip = process.platform !== 'win32' ? ' (Ctrl + C to skip)' : ' (Cmd + C to skip)';
 
-	// Once we're done, finalize the YAML file
-	finalize();
+	var schema = {
+		properties: {
+			environ     : {
+				description: 'Name of the environment',
+				default    : defaults.environ,
+				message    : 'Environment must be a string',
+				type       : 'string'
+			},
+			hhvm_domains: {
+				description: 'HHVM project domains' + skip,
+				default    : defaults.hhvm_domains,
+				message    : 'HHVM project domains' + skip,
+				type       : 'array'
+			},
+			php_domains : {
+				description: 'PHP project domains' + skip,
+				default    : defaults.php_domains,
+				message    : 'PHP project domains' + skip,
+				type       : 'array'
+			},
+			wpe_name    : {
+				description: 'Name of the WPEngine Installation',
+				default    : defaults.wpe_name,
+				message    : 'Environment must be a string',
+				type       : 'string'
+			},
+			branch      : {
+				description: 'Git branch name',
+				default    : defaults.branch,
+				message    : 'Branch name must be a string',
+				type       : 'string'
+			},
+			remote      : {
+				description: 'Git remote name',
+				default    : defaults.remote,
+				message    : 'Remote name must be a string',
+				type       : 'string'
+			}
+		}
+	};
+
+	prompt.start();
+	prompt.get( schema, function( err, result ) {
+		// Set up our settings array
+		settings = { wp: result };
+
+		// Once we're done, finalize the YAML file
+		finalize();
+	} );
 }
 
 /**
