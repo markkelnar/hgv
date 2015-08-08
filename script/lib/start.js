99596345581fa5bafda9ead15170927341b52be7
Eric Mann <eric@eamann.com>
2015-08-08 16:57:21 -0700

    Move scripts around to make things a bit more consistent

---
+++ script/lib/start.js
@@ -0,0 +1,215 @@
+/**
+ * Hgv YAML Configuration Generator
+ *
+ * @author WPEngine
+ */
+
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var fs = require( 'fs' ),
+	path = require( 'path' ),
+	extend = require( 'util' )._extend,
+	chalk = require( 'chalk' ),
+	prompt = require( 'prompt' ),
+	rootCheck = require( 'root-check' ),
+	yaml = require( 'yamljs' );
+
+/**
+ * Default values for the YAML file
+ */
+var defaults = {
+	enviro      : '',
+	hhvm_domains: [],
+	php_domains : [],
+	wpe_name    : '',
+	branch      : 'develop',
+	remote      : 'staging'
+};
+
+/**
+ * Object container that will be serialized to YAML.
+ */
+var settings = {};
+
+/**
+ * Command line arguments
+ */
+var argv = process.argv.slice( 2 );
+
+/**
+ * Just some fun ASCII art for Hgv consistency
+ */
+function pre() {
+	console.log( require( './header' ) );
+
+	// Check to see if our config file exists. If so, only proceed if --force is set.
+	fs.stat( '.mercuryrc', function( err, stat ) {
+		if ( err !== null ) {
+			// The file doesn't exist, so start things up.
+			init();
+		} else {
+			process.stdout.write( '\n' );
+			process.stdout.write( '    ' + chalk.red( 'WARNING!' ) + '\n' );
+			process.stdout.write( '    ' + chalk.yellow( 'Your installation already has a ' + chalk.cyan( '.mercuryrc' ) + ' file.\n' ) );
+
+			// The file exists! Check for the --force flag.
+			if ( argv.indexOf( '--force' ) > -1 ) {
+				process.stdout.write( '    ' + chalk.yellow( 'This utility will update the existing file\'s settings.\n' ) );
+
+				// Parse the existing file and store it in the default array
+				yaml.load( '.mercuryrc', function( result ) {
+					console.log( result );
+
+					// Merge our parameters
+					settings = result;
+					defaults = extend( defaults, result.wp );
+
+					// Start things up
+					init();
+				} );
+			} else {
+				// Explain how to use the tool.
+				process.stdout.write( '\n' );
+				process.stdout.write( '    To overwrite this file, add the ' + chalk.bold( '--force' ) + ' flag to the ' + chalk.bold( '/script/start' ) + '\n' );
+				process.stdout.write( '    command to re-run the generator and update the existing values.\n' );
+
+				// Exit with an error code.
+				process.exit( 1 );
+			}
+		}
+	} );
+}
+
+/**
+ * Make sure no comma-separated lists end up in an array.
+ *
+ * @param {Array} array Array to clean up.
+ *
+ * @returns {Array}
+ */
+function fix_arrays( array ) {
+	var output_array = [];
+
+	for ( var i = 0, l = array.length; i < l; i++ ) {
+		var domain = array[ i ];
+		if ( domain.indexOf( ',' ) > -1 ) {
+			var domains = domain.split( ',' );
+			output_array = output_array.concat( domains );
+		} else {
+			output_array.push( domain );
+		}
+	}
+
+	return output_array;
+}
+
+/**
+ * Return a de-duped version of an array.
+ *
+ * @param {Array} array Array to de-dupe
+ *
+ * @returns {Array}
+ */
+function uniq( array ) {
+	var seen = {};
+	return array.filter( function ( item ) {
+		return seen.hasOwnProperty( item ) ? false : (seen[item] = true);
+	} );
+}
+
+/**
+ * Initialize the prompting machine.
+ */
+function init() {
+	var skip = process.platform !== 'win32' ? ' (Ctrl + C to skip)' : ' (Cmd + C to skip)';
+
+	var schema = {
+		properties: {
+			environ     : {
+				description: 'Name of the environment',
+				default    : defaults.environ,
+				message    : 'Environment must be a string',
+				type       : 'string'
+			},
+			hhvm_domains: {
+				description: 'HHVM project domains' + skip,
+				default    : defaults.hhvm_domains,
+				message    : 'HHVM project domains' + skip,
+				type       : 'array'
+			},
+			php_domains : {
+				description: 'PHP project domains' + skip,
+				default    : defaults.php_domains,
+				message    : 'PHP project domains' + skip,
+				type       : 'array'
+			},
+			wpe_name    : {
+				description: 'Name of the WPEngine Installation',
+				default    : defaults.wpe_name,
+				message    : 'Environment must be a string',
+				type       : 'string'
+			},
+			branch      : {
+				description: 'Git branch name',
+				default    : defaults.branch,
+				message    : 'Branch name must be a string',
+				type       : 'string'
+			},
+			remote      : {
+				description: 'Git remote name',
+				default    : defaults.remote,
+				message    : 'Remote name must be a string',
+				type       : 'string'
+			}
+		}
+	};
+
+	prompt.start();
+	prompt.get( schema, function( err, result ) {
+		// Split up any comma-separated lists in the arrays
+		result.hhvm_domains = uniq( fix_arrays( result.hhvm_domains ) );
+		result.php_domains = uniq( fix_arrays( result.php_domains ) );
+
+		// Set up our settings array
+		settings = { wp: result };
+
+		// Once we're done, finalize the YAML file
+		finalize();
+	} );
+}
+
+/**
+ * Write out our generated YAML file.
+ */
+function finalize() {
+	var yaml_string = yaml.stringify( settings, 4 );
+
+	fs.writeFile( '.mercuryrc', yaml_string, function( err ) {
+		if ( err ) {
+			process.stderr.write( err.toString() );
+		} else {
+			process.stdout.write( '    ' + chalk.green( 'All settings sucessfully saved!\n' ) );
+			process.exit();
+		}
+	} );
+}
+
+/**
+ * If the user is running as root, warn them
+ */
+rootCheck(
+	'\n' +
+	chalk.red('Be careful using the `sudo` command.') +
+	'\n\nSince `start` is a user command, there is no need to execute it with root\n' +
+	'permissions. If you\'re having permission errors when running without sudo,\n' +
+	'please spend a few minutes learning more about how your system should work\n' +
+	'and make any necessary repairs.'
+);
+
+/**
+ * Fire off the generator
+ */
+pre();
--- script/start.cmd
+++ script/start.cmd
@@ -1,7 +1,7 @@
 @IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\start" %*
+  "%~dp0\node.exe"  "%~dp0\lib\start.js" %*
 ) ELSE (
   @SETLOCAL
   @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\start" %*
+  node  "%~dp0\lib\start.js" %*
 )
