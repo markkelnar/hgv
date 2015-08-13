d836b9a09b9859f98e88a71f42bc0869a6870b1d
Eric Mann <eric@eamann.com>
2015-08-13 12:06:36 -0700

    Add space below the header

--- script/lib/header.js
+++ script/lib/header.js
@@ -25,7 +25,9 @@ var fallback = [
 	chalk.gray( '|          |___/( _ )/  \\ |' ) + chalk.green( '|       |_  )__ / |' ),
 	chalk.gray( '|               / _ \\ () ||' ) + chalk.green( '|        / / |_ \\ |' ),
 	chalk.gray( '|               \\___/\\__/ |' ) + chalk.green( '|       /___|___/ |' ),
-	chalk.gray( ' ------------------------- ' ) + chalk.green( ' ----------------- ' )
+	chalk.gray( ' ------------------------- ' ) + chalk.green( ' ----------------- ' ),
+	'',
+	''
 ].join( '\n' );
 
 module.exports = fallback;
