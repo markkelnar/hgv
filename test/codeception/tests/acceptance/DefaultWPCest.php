3b0a69df734025bf3c666444077e3201bef05ad3
Mark Kelnar <mark@wpengine.com>
2015-08-19 16:11:45 -0500

    Add test for file upload size

--- test/codeception/tests/acceptance/DefaultWPCest.php
+++ test/codeception/tests/acceptance/DefaultWPCest.php
@@ -1,6 +1,7 @@
 <?php
 use \AcceptanceTester;
 
+// TODO: These tests are duplicate for hhvm and php. Probably a good idea to refactor these tests so there is one set of tests and each domain endpoint is extended to call the test suite.
 class DefaultWPCest
 {
     public function _before(AcceptanceTester $I)
@@ -28,4 +29,35 @@ class DefaultWPCest
         $I->seeResponseCodeIs(200);
     }
 
+    /**
+     * This test will check the file upload size limit in wp-admin media uploader
+     */
+    public function checkFileUploadLimitHHVM(AcceptanceTester $I)
+    {
+        $I->wantTo('Log into site and check file upload on media page');
+        $I->amOnUrl('http://hhvm.hgv.test');
+        $I->amOnPage('/wp-login.php');
+        $I->fillField('log', 'wordpress');
+        $I->fillField('pwd', 'wordpress');
+        $I->click('Log In');
+
+        $I->amOnPage('/wp-admin/media-new.php');
+        $I->see('Maximum upload file size: 200 MB.');
+    }
+
+    /**
+     * This test will check the file upload size limit in wp-admin media uploader
+     */
+    public function checkFileUploadLimitPHP(AcceptanceTester $I)
+    {
+        $I->wantTo('Log into site and check file upload on media page');
+        $I->amOnUrl('http://php.hgv.test');
+        $I->amOnPage('/wp-login.php');
+        $I->fillField('log', 'wordpress');
+        $I->fillField('pwd', 'wordpress');
+        $I->click('Log In');
+
+        $I->amOnPage('/wp-admin/media-new.php');
+        $I->see('Maximum upload file size: 200 MB.');
+    }
 }
