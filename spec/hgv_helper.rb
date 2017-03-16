191bbb4b8b162ac764ad9b53d6c63a92c4f4b2c5
Mark Kelnar <mark@wpengine.com>
2017-03-16 13:58:43 -0500

    HGV helper spec tests should be vagrant/circleci aware

--- spec/hgv_helper.rb
+++ spec/hgv_helper.rb
@@ -15,18 +15,20 @@ end
 
 host = ENV['TARGET_HOST']
 
-`vagrant up #{host}`
+if !ENV['CI']
+  `vagrant up #{host}`
 
-config = Tempfile.new('', Dir.tmpdir)
-config.write(`vagrant ssh-config #{host}`)
-config.close
+  config = Tempfile.new('', Dir.tmpdir)
+  config.write(`vagrant ssh-config #{host}`)
+  config.close
 
-options = Net::SSH::Config.for(host, [config.path])
+  options = Net::SSH::Config.for(host, [config.path])
 
-options[:user] ||= Etc.getlogin
+  options[:user] ||= Etc.getlogin
 
-set :host,        options[:host_name] || host
-set :ssh_options, options
+  set :host,        options[:host_name] || host
+  set :ssh_options, options
+end
 
 # Disable sudo
 # set :disable_sudo, true
