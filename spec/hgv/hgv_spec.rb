510327a949740ca1f313049977a6382b8ebd62d0
Mark Kelnar <mark@wpengine.com>
2017-03-17 08:18:51 -0500

    sudo to check mysql status

--- spec/hgv/hgv_spec.rb
+++ spec/hgv/hgv_spec.rb
@@ -27,19 +27,30 @@ end
 #
 services = %w{
   nginx
-  mysql
   hhvm
   varnish
 }
 
 services.each do |service|
   describe service(service) do
-    let(:disable_sudo) { false }
     it { should be_enabled }
     it { should be_running }
   end
 end
 
+#
+# mysql service running
+# Because of service mysql status permissions denied on pid lock file and
+# 'status' doesn't return with 'running' string in the output,
+# do it this way
+#
+describe command('sudo service mysql status') do
+  its(:stdout) { should match /Uptime:/ }
+end
+
+describe process("mysqld") do
+  it { should be_running }
+end
 
 #
 # Web apps are working?
