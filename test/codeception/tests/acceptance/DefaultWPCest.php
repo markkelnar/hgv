3fbdccdd6f136f769137e65f150ad0896f23c3f7
Mark Kelnar <mark@wpengine.com>
2017-04-04 15:13:09 -0500

    Add tests around php56 cookie

--- test/codeception/tests/acceptance/DefaultWPCest.php
+++ test/codeception/tests/acceptance/DefaultWPCest.php
@@ -13,12 +13,13 @@ class DefaultWPCest
     }
 
     // tests
-    public function viewPageHHVM(AcceptanceTester $I)
+    public function viewPageHHVMNoCookie(AcceptanceTester $I)
     {
         $I->amOnUrl('http://hhvm.hgv.test');
         $I->amOnPage('/');
         $I->seeCurrentUrlEquals('/');
         $I->seeResponseCodeIs(200);
+        $I->assertRegExp('#HHVM(.*)#', $I->grabHttpHeader('X-Powered-By'));
     }
 
     public function viewPageHHVMcache(AcceptanceTester $I)
@@ -45,8 +46,52 @@ class DefaultWPCest
         $I->seeResponseCodeIs(200);
     }
 
+    // Test that can load the default WP with PHP 5.5
+    public function viewPageCookiePHP(AcceptanceTester $I)
+    {
+        $I->setCookie('backend', 'php');
+        $I->amOnUrl('http://hhvm.hgv.test');
+        $I->amOnPage('/');
+        $I->seeCurrentUrlEquals('/');
+        $I->seeResponseCodeIs(200);
+        $I->assertRegExp('#PHP/5\.5\.(.*)#', $I->grabHttpHeader('X-Powered-By'));
+    }
+
+    // Test that can load the default WP with PHP 5.5
+    public function viewPageCookiePHP5(AcceptanceTester $I)
+    {
+        $I->setCookie('backend', 'php5');
+        $I->amOnUrl('http://hhvm.hgv.test');
+        $I->amOnPage('/');
+        $I->seeCurrentUrlEquals('/');
+        $I->seeResponseCodeIs(200);
+        $I->assertRegExp('#PHP/5\.5\.(.*)#', $I->grabHttpHeader('X-Powered-By'));
+    }
+
+    // Test that can load the default WP with PHP 5.5
+    public function viewPageCookiePHP55(AcceptanceTester $I)
+    {
+        $I->setCookie('backend', 'php55');
+        $I->amOnUrl('http://hhvm.hgv.test');
+        $I->amOnPage('/');
+        $I->seeCurrentUrlEquals('/');
+        $I->seeResponseCodeIs(200);
+        $I->assertRegExp('#PHP/5\.5\.(.*)#', $I->grabHttpHeader('X-Powered-By'));
+    }
+
+    // Test that can load the default WP with PHP 5.6
+    public function viewPageCookiePHP56(AcceptanceTester $I)
+    {
+        $I->setCookie('backend', 'php56');
+        $I->amOnUrl('http://hhvm.hgv.test');
+        $I->amOnPage('/');
+        $I->seeCurrentUrlEquals('/');
+        $I->seeResponseCodeIs(200);
+        $I->assertRegExp('#PHP/5\.6\.(.*)#', $I->grabHttpHeader('X-Powered-By'));
+    }
+
     // Test that can load the default WP with PHP 7
-    public function viewPagePHP7(AcceptanceTester $I)
+    public function viewPageCookiePHP7(AcceptanceTester $I)
     {
         $I->setCookie('backend', 'php7');
         $I->amOnUrl('http://hhvm.hgv.test');
@@ -54,6 +99,17 @@ class DefaultWPCest
         $I->seeCurrentUrlEquals('/');
         $I->seeResponseCodeIs(200);
         $I->assertRegExp('#PHP/7\.(.*)#', $I->grabHttpHeader('X-Powered-By'));
+    }
+
+    // Test that can load the default WP with HHVM
+    public function viewPageCookieHHVM(AcceptanceTester $I)
+    {
+        $I->setCookie('backend', 'hhvm');
+        $I->amOnUrl('http://hhvm.hgv.test');
+        $I->amOnPage('/');
+        $I->seeCurrentUrlEquals('/');
+        $I->seeResponseCodeIs(200);
+        $I->assertRegExp('#HHVM(.*)#', $I->grabHttpHeader('X-Powered-By'));
     }
 
     /**
